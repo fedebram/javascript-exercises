af2abc50416814eb295d5d7f31ffad6a762183ba
fedebram <163430291+fedebram@users.noreply.github.com>
2024-03-28 10:54:18 +0100

    Esercizio svolto con successo

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let messaggio = "";
+    if (times < 0){
+        return "ERROR";
+    }
+    for (let i = 0; i < times; i++){
+        messaggio = messaggio + word;
+    }
+    return messaggio;
 };
 
 // Do not edit below this line
