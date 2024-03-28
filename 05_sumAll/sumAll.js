8c064753d640d4fdc0a222f59604bd33beca0d28
fedebram <163430291+fedebram@users.noreply.github.com>
2024-03-28 16:01:37 +0100

    Esercizio svolto con successo

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    let somma = 0;
+    if ((!Number.isInteger(min)) || (!Number.isInteger(max)) || (min < 0) || (max < 0)){
+        return `ERROR`;
+    }
+    else if (max < min){
+        for (i= max; i <= min; i++){
+            somma += i;
+        }
+    }else {
+        for (i= min; i <= max; i++){
+        somma += i;
+        }
+    }
+    return somma;
 };
 
 // Do not edit below this line
