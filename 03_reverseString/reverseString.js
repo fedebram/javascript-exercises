51d7094a50978058cd3b667bdf280d34815bc189
fedebram <163430291+fedebram@users.noreply.github.com>
2024-03-28 12:08:05 +0100

    Esercizio svolto con successo

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let messaggio = "";
+    const caratteri = Array.from(word);
+    for (let i = caratteri.length-1; i >= 0; i--){
+        messaggio += caratteri[i];
+    }
+    return messaggio;
 };
 
 // Do not edit below this line
