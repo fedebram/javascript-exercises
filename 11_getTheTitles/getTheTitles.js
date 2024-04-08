0a15dd581c9a1ee010c337c5fb65879bd8deb3a3
fedebram <163430291+fedebram@users.noreply.github.com>
2024-04-08 23:10:32 +0200

    done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let arr = [];
+    let count = 0;
+    books.map((book) => {
+        arr.splice(count, 0, book.title);
+        count ++;
+    });
+    return arr;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let lifeSpan = 0;
+    let maxLife = 0;
+    let count = 0;
+    for (i = 0; i < people.length; i++) {
+        if (people[i].yearOfDeath === undefined) people[i].yearOfDeath = new Date().getFullYear(); //in case the year of death is absent
+        lifeSpan = (people[i].yearOfDeath - people[i].yearOfBirth);
+        if (maxLife < lifeSpan) {
+        maxLife = lifeSpan;
+            if (i !== 0) count ++;
+        }
+    }
+    return people[count];
 
 };
 
