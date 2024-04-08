9129a9da43e0f581437528ac051989652e9348a7
fedebram <163430291+fedebram@users.noreply.github.com>
2024-04-08 19:37:59 +0200

    exercise complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    let arr = [0, 1];
+    for  (let i = 2; i <= member; i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    if (member < 0) return "OOPS";
+    return arr[member];
 };
 
 // Do not edit below this line
